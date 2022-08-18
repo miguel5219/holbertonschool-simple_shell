e8fd7deef1d74d8757157da60283c95a097373aa
miguel5219 <5219@holbertonschool.com>
2022-08-17 20:45:41 -0700

    add fork_process

--- main.c
+++ main.c
@@ -25,23 +25,26 @@ int main(void)
 		l_token = malloc(sizeof(char *) * (token_counter(line, " \n\t") + 1));
 		l_token = tokenizer(line, " \n\t");
 		if (!l_token)
-		{
-			perror("./hsh");
+		{	perror("./hsh");
 			free(line);
-			continue;
-		}
+			continue; }
 		if (access(l_token[0], F_OK) == -1)
-		{
-			path = get_env(l_token[0]);
+		{	path = get_env(l_token[0]);
 			if (!path)
-			{
-				perror("./hsh");
+			{	perror("./hsh");
 				free(path);
-				free_ptr(l_token);
-				continue;
-			}
+				_free_ptr(l_token);
+				continue; }
 		}
 		else
 			path = str_dup(l_token[0]);
-	}
+
+		if (fork_process(l_token, path) == -1)
+		{	perror("./hsh");
+			free(l_token);
+			free(path);
+			free(line);
+			continue; }
+		free(line); }
+	return (0);
 }
