35fcb8e6047030ea722e9370c11962a6657df943
Jhonatan Ramos <jhonatan.ramox@gmail.com>
2022-08-17 20:45:07 -0500

    Add funtion tokenizer.c

---
+++ tokenizer.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * tokenizer - Separate the string into segments
+ * @string: string to tokenize
+ * @delimiter: delimiters characters 
+ * Return: tokens
+ */
+
+char **tokenizer(char *string, char *delimiter)
+{
+	int i = 1;
+	int lenght = 0;
+	char **tokens = NULL;
+
+	lenght = token_counter(string, delimiter);
+	tokens = malloc(sizeof(char *) * (lenght + 1));
+	if (!tokens)
+		return (NULL);
+
+	tokens[0] = strtok(string, delimiter);
+	if (!tokens[0])
+	{
+		_free_ptr(tokens);
+		return (NULL);
+	}
+
+	while (i < lenght)
+	{
+		tokens[i] = strtok(NULL, delimiter);
+		i++;
+	}
+	return (tokens);
+}
